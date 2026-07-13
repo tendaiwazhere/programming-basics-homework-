2656919c27709c69c7f89b2c59295a32285b1da0
tendaiwazhere <apuulitendai@gmail.com>
2026-07-13 16:56:41 -0500

    initial commit

---
+++ part-a-hello/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("4!\n");
+    return 0;
+}
+
+int main(void) {
+    printf("0.45!\n");
+    return 0;
+}
---
+++ part-b-basics/basics.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "0.45,4,(\"PascalCase!\")" << std::endl;
+    return 0;
+}
